3513f8b3af0e46bdcbf61341e9792f50234701d8
Kelly Appleton <kda9036@rit.edu>
2024-09-10 19:07:01 -0400

    eliminate extra variable

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -26,7 +26,7 @@ export default function Examples() {
   return (
     <Section title="Examples" id="examples">
       <Tabs
-        buttonsContainer="menu"
+        ButtonsContainer="menu"
         buttons={
           <>
             <TabButton
